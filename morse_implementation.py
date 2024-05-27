872666c5d0e8a5c986fb475c45dc1fd1bdad988b
GijsEltinkTUe <g.a.w.eltink@student.tue.nl>
2024-05-27 16:45:27 +0200

    Update morse_implementation.py

--- morse_implementation.py
+++ morse_implementation.py
@@ -21,50 +21,69 @@ x, y = np.meshgrid(range(z.shape[0]), range(z.shape[1]))
 def edge_value(coord_x, coord_y, dir, eps=1e-8):
     """determines morse func value of edge with 
     direction True == Right (and false is downwards)
+    is_max True => left/top vertex was maximal
     """
     point = data_points[coord_x][coord_y]
     if dir:
         second_point = data_points[coord_x+1][coord_y]
     else:
         second_point = data_points[coord_x][coord_y+1]
-    return max(point, second_point) + eps*min(point, second_point)
+    return max(point, second_point) + eps*min(point, second_point), point >= second_point, 0
 
 print(data_points[10][15], data_points[10][16], data_points[11][15])
-print(edge_value(10,15,True))
+# print(edge_value(10,15,True))
 
 # horizontal_edges = np.array([edge_value(coord_x, coord_y, 1) for coord_x, coord_y in zip(np.arange(1600-1), np.arange(160-1))])
-horizontal_edges = np.zeros((1600-1, 160))
+# horizontal_edges = np.zeros((1600-1, 160))
+horizontal_edges = np.empty((1600-1, 160),object)
 for coord_x in range(1600-1):
     for coord_y in range(160):
         horizontal_edges[coord_x][coord_y] = edge_value(coord_x, coord_y, 1)
 
 # vertical_edges = [edge_value(coord_x, coord_y, 0) for coord_x, coord_y in zip(np.arange(1600-1), np.arange(160-1))]
-vertical_edges = np.zeros((1600, 160-1))
+# vertical_edges = np.zeros((1600, 160-1))
+vertical_edges = np.empty((1600, 160-1),object)
 for coord_x in range(1600):
     for coord_y in range(160-1):
-        vertical_edges[coord_x][coord_y] = edge_value(coord_x, coord_y, 0)
+        vertical_edges[coord_x][coord_y] = edge_value(coord_x, coord_y, 1)
 
 print(horizontal_edges[10][15])
 
 def cell_value(coord_x, coord_y, eps = 1e-8):
     """Determines cell value based on edges around it
     Max edge value + eps^2 * opposite (not min)
+    dir_max : 11 top, 10 bottom, 01 left, 00 right
     """
-    hor_edges = horizontal_edges[coord_x][coord_y], horizontal_edges[coord_x][coord_y+1]
-    ver_edges = vertical_edges[coord_x][coord_y], vertical_edges[coord_x+1][coord_y]
+    hor_edges = horizontal_edges[coord_x][coord_y][0], horizontal_edges[coord_x][coord_y+1][0]
+    ver_edges = vertical_edges[coord_x][coord_y][0], vertical_edges[coord_x+1][coord_y][0]
 
     options = hor_edges + ver_edges
+
     max_edge = np.argmax(options)
     if max_edge % 2 == 0:
-        min_edge = max_edge + 1
+        opp_edge = max_edge + 1
     else:
-        min_edge = max_edge - 1
-    return options[max_edge] + eps**2 *  options[min_edge]
+        opp_edge = max_edge - 1
+    # if coord_y == 150 and coord_x == 1500:
+    # print(max_edge, opp_edge, options)
+    return options[max_edge] + eps**2 *  options[opp_edge], "{:02b}".format(3 - max_edge)
 
-cell_values = np.zeros((1600-1, 160-1))
+# cell_values = np.zeros((1600-1, 160-1))
+cell_values = np.empty((1600-1, 160-1),object)
 for coord_x in range(1600-1):
     for coord_y in range(160-1):
         cell_values[coord_x][coord_y] = cell_value(coord_x, coord_y)
+        if coord_x == 1500 and coord_y == 100:
+            print(horizontal_edges[coord_x][coord_y])
 
 print(cell_values[0][0])
 print(np.min(horizontal_edges), np.min(vertical_edges), np.min(cell_values))
+
+is_minimum = np.ones((1600, 160))
+
+def draw_vertex_edge_pair(coord_x, coord_y):
+    """Determines for a vertex whether it is the maximum of an adjacent edge.
+    If it is, determine for all edges of which it is a maximum, add a gradient pair to the smallest edge.
+    0 => minimum (no larger edges), and 1 through 4 for clockwise pairs (starting top)
+    """
+    

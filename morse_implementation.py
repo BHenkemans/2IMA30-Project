Refactor edge-cell gradient function. + Al een bug gevonden die gefixt moet worden

morse_implementation.py
@@ -141,43 +141,35 @@ def draw_edge_cell_pair(array, coord_x, coord_y):
     """
     
     # If we already know the edge is not a saddle by vertex-edge gradient pairs, we can skip this edge
-    if (array[coord_x][coord_y][2] == 0):
+    if array[coord_x][coord_y][2] == 0:
         return
     
     gradient_so_far = 0
     gradient_min_value = np.inf
     
-    if array is horizontal_edges:
-        if coord_y > 0:
-            cell_value, cell_status = cell_values[coord_x][coord_y-1]
-            if cell_status == "10" and cell_value < gradient_min_value:
-                gradient_so_far = 1
-                gradient_min_value = cell_value
-        if coord_y < 160-1:
-            cell_value, cell_status = cell_values[coord_x][coord_y]
-            if cell_status == "11" and cell_value < gradient_min_value:
-                gradient_so_far = 2
-                gradient_min_value = cell_value
-        
-    elif array is vertical_edges:
-        if coord_x > 0:
-            cell_value, cell_status = cell_values[coord_x-1][coord_y]
-            if cell_status == "00" and cell_value < gradient_min_value:
-                gradient_so_far = 3
-                gradient_min_value = cell_value
-        if coord_x < 1600-1:
-            cell_value, cell_status = cell_values[coord_x][coord_y]
-            if cell_status == "01" and cell_value < gradient_min_value:
-                gradient_so_far = 4
+    # Define a list of conditions and corresponding actions
+    conditions = [
+        (array is horizontal_edges and coord_y > 0, (coord_x, coord_y-1), "10", 1),
+        (array is horizontal_edges and coord_y < 160-1, (coord_x, coord_y), "11", 2),
+        (array is vertical_edges and coord_x > 0, (coord_x-1, coord_y), "00", 3),
+        (array is vertical_edges and coord_x < 1600-1, (coord_x, coord_y), "01", 4)
+    ]
+
+    for condition, coords, cell_status_expected, gradient in conditions:
+        if condition:
+            cell_value, cell_status_actual = cell_values[coords]
+            if cell_status_actual == cell_status_expected and cell_value < gradient_min_value:
+                gradient_so_far = gradient
                 gradient_min_value = cell_value
                 
     if gradient_so_far > 0:
         is_maximum[coord_x][coord_y] = 0
-        if gradient_so_far == 1 or gradient_so_far == 2:
+        if gradient_so_far in [1, 2]:
             horizontal_edges[coord_x][coord_y][2] = 0
-        elif gradient_so_far == 3 or gradient_so_far == 4:
+        elif gradient_so_far in [3, 4]:
             vertical_edges[coord_x][coord_y][2] = 0
 
+# horizontal_edges heeft shape (1600-1, 160), vertical_edges heeft shape (1600, 160-1). Dit dus nog fixen dat hij m volledig doet.
 for coord_x in range(1600-1):
     for coord_y in range(160-1):
         draw_edge_cell_pair(horizontal_edges, coord_x, coord_y)